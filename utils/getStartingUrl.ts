a850ae8d9f5891a5f05540b984819b87facd27ed
Linkerin <130695447+Linkerin@users.noreply.github.com>
2025-01-09 00:14:20 +0300

    fix: 🐛 Fixed recursive search for the docs starting page. Previously it didn't check visited links set.

--- utils/getStartingUrl.ts
+++ utils/getStartingUrl.ts
@@ -6,32 +6,28 @@ import gotoWithRetry from './gotoWithRetry';
 import { logger } from '../services/Logger';
 
 interface GetNextUrlParams {
-  initialSearch?: boolean;
   page: Page;
   url: URL;
 }
 
-async function getStartingUrl({
-  initialSearch = true,
-  page,
-  url
-}: GetNextUrlParams): Promise<URL | null> {
+async function getStartingUrl(
+  { page, url }: GetNextUrlParams,
+  visited?: Set<string>
+): Promise<URL | null> {
   await gotoWithRetry(page, url.href);
   const nextUrl = await getNextUrl(page);
 
   if (nextUrl) return url;
 
-  if (!initialSearch) return null;
-
   // recursive search for the page to start parsing
   const links = await page.$$eval('a', links => links.map(link => link.href));
   if (links.length === 0) return null;
 
-  const visitedLinks = new Set<string>();
+  const visitedLinks = visited ?? new Set<string>();
   visitedLinks.add(url.href);
 
   for (const link of links) {
-    if (!link) continue;
+    if (!link || visitedLinks.has(link)) continue;
     if (!URL.canParse(link, url.origin)) continue;
 
     const nextUrl = new URL(link, url.origin);
@@ -44,16 +40,13 @@ async function getStartingUrl({
     );
 
     visitedLinks.add(nextUrl.href);
-    const nextResult = await getStartingUrl({
-      url: nextUrl,
-      initialSearch: false,
-      page
-    });
+    const nextResult = await getStartingUrl(
+      { url: nextUrl, page },
+      visitedLinks
+    );
 
     if (nextResult) return nextResult;
   }
-
-  logger.info('No more pages found.');
 
   return null;
 }
